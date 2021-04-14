976b557421006fa384ad9b7e2bbeaec267c7c607
Jaxson <ruffjaxson@gmail.com>
2021-04-14 08:54:08 -0600

    changed port

--- back-end/server.js
+++ back-end/server.js
@@ -140,4 +140,4 @@ app.delete('/api/arguments/:argumentID/comments/:commentID', async (req, res) =>
 });
 
 
-app.listen(3000, () => //console.log('Server listening on port 3000!'));
+app.listen(3001, () => console.log('Server listening on port 3001!'));
